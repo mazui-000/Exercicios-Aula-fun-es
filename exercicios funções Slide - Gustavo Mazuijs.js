319cc7bec3681c9cf12588608a40e8f7fa43b1da
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-25 10:36:02 -0300

    Add files via upload

---
+++ exercicios funções Slide - Gustavo Mazuijs.js
@@ -0,0 +1,66 @@
+////////////////////////////////////* Exercícios Slide 1 
+
+/* function imprimirOlaMundo() {
+    console.log ("Olá Mundo!")
+}
+
+imprimirOlaMundo () /*/
+
+
+////////////////////////////////////* Exercícios Slide 2 
+
+ /* function imprimirNome(nome) {
+    console.log (`Olá ${nome}`)
+}
+
+let nome = "Gustavo"
+imprimirNome(nome)
+
+imprimirNome("Mazui")
+imprimirNome("gu")
+imprimirNome("sei lákkkk") */
+
+////////////////////////////////////* Exercícios Slide 3 
+
+  /* function calcularnumeros (x,y) {
+    let soma = x+y 
+    return soma 
+ }
+
+const resultadoSoma = calcularnumeros (4, 9)
+console.log (calcularnumeros (4, 9)) */ 
+
+////////////////////////////////////* Exercícios Slide 4 
+
+/* let array = [2, 4, 6, 8]
+ function recebo(array) {
+    let array2 = []
+    let a = array [0] / 2 
+    let b = array [3] / 2 
+    array.push (a, b)
+    return array
+ }
+
+ console.log(recebo(array)) */ 
+
+////////////////////////////////////* Exercícios Slide 5 
+
+//* exercicio 2 
+
+
+/* let imprimirNome2 = function(nome2) {
+    console.log (`Olá ${nome2}`)
+}
+
+imprimirNome2 (nome) */ 
+
+
+
+//* exercicio 3
+
+ /* let calcularnumeros = function (x,y) {
+    return x+y 
+ }
+
+const resultadoSoma = calcularnumeros (4, 9)
+console.log (calcularnumeros (4, 9)) */ 
